e69c37b3105eaff6f0127acfbce3177423e2e7c5
danembb <dbrophy720@gmail.com>
2021-04-19 13:38:00 -0600

    Update sales_engine and continue refactor for invoice_item_repository_spec

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,19 +1,62 @@
 require 'CSV'
 require_relative 'item_repo'
 require_relative 'merchant_repo'
+require_relative 'sales_analyst'
+require_relative 'invoice_repo'
+require_relative 'invoice_item_repo'
+require_relative 'transaction_repo'
+require_relative 'customer_repo'
 
 class SalesEngine
   attr_reader :items,
-              :merchants
+              :merchants,
+              :analyst,
+              :invoices,
+              :invoice_items,
+              :transactions,
+              :customers
+
   def initialize(paths)
-    @items = ItemRepo.new(paths[:items], self)
-    @merchants = MerchantRepo.new(paths[:merchants], self)
+   @items = ItemRepo.new(paths[:items], self)
+   @merchants = MerchantRepo.new(paths[:merchants], self)
+   @analyst = SalesAnalyst.new(self)
+   @invoices = InvoiceRepo.new(paths[:invoices], self)
+   @invoice_items = InvoiceItemRepo.new(paths[:invoice_items], self)
+   @transactions = TransactionRepo.new(paths[:transactions], self)
+   @customers = CustomerRepo.new(paths[:customers], self)
   end
 
   def self.from_csv(paths)
     new(paths)
   end
-  # self.from_csv(hash from i0 directions) => paths
-  # from csv method will call initialize to pass 
-  # the hash through, should return the instance of itself 
+
+  def all_items #Needs spec
+    @items.all
+  end
+
+  def all_merchants #Needs spec
+    @merchants.all
+  end
+
+  def item_count #Needs spec
+    count = @items.all.length
+    count.to_f
+  end
+
+  def merchant_count #Needs spec
+    count = @merchants.all.length
+    count.to_f
+  end
+
+  def average_item_price #Needs spec
+    @items.average_price
+  end
+
+  def item_count_per_merchant #Needs spec
+    @items.item_count_per_merchant
+  end
+
+  def find_merchant_id(id) #Needs spec
+    @items.find_all_by_merchant_id(id)
+  end
 end
